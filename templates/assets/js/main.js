2a150d1a8169c01d412e542fa81707053cb1b950
1152958806@qq.com <1152958806@qq.com>
2023-08-12 21:20:12 +0800

    侧边栏 - profile（站点资料）, comments（最新评论）, tags（文章标签）

---
+++ templates/assets/js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    /**
+     * 網頁運行時間
+     */
+    const addRuntime = () => {
+        const $runtimeCount = document.getElementById('runtimeshow');
+        if ($runtimeCount) {
+            var s1 = $runtimeCount.innerText;;//建站时间
+            if (s1) {
+                s1 = new Date(s1.replace(/-/g, "/"));
+                s2 = new Date();
+                var days = s2.getTime() - s1.getTime();
+                var number_of_days = parseInt(days / (1000 * 60 * 60 * 24));
+                $runtimeCount.innerText = number_of_days + "天";
+            }
+        }
+    }
+    window.refreshFn = function () {
+        addRuntime();
+    }
+    refreshFn()
+})
